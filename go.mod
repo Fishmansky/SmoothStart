5a25d1eca35246fc5e41d54e03ad4191cd3a451a
Fishmansky <paul96guitar@gmail.com>
2024-07-17 19:13:08 +0200

    go.mod, go.sum and views updates

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.22.3
 
 require (
 	github.com/a-h/templ v0.2.707
-	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
@@ -15,6 +16,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/echo-jwt/v4 v4.2.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- views/admin/header_templ.go
+++ views/admin/header_templ.go
@@ -23,7 +23,7 @@ func Header() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"bg-mywhite px-12 flex justify-between\"><h1 class=\"p-4 text-dark text-2xl font-semibold\">SmoothStart</h1><div class=\"flex justify-end place-items-center\"><a class=\"p-2 cursor-pointer font-medium text-xl\" href=\"/admin/home\" hx-push-url=\"true\">Home</a> <a class=\"p-2 cursor-pointer font-medium text-xl\" href=\"/admin/team\" hx-push-url=\"true\">Team</a> <a class=\"p-2 cursor-pointer font-medium text-xl\" href=\"/admin/plans\" hx-push-url=\"true\">Plan</a></div></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"bg-mywhite px-12 flex justify-between\"><h1 class=\"p-4 text-dark text-2xl font-semibold\">SmoothStart</h1><div class=\"flex justify-end place-items-center\"><a class=\"p-2 cursor-pointer font-medium text-xl\" href=\"/admin/home\" hx-push-url=\"true\">Home</a> <a class=\"p-2 cursor-pointer font-medium text-xl\" href=\"/admin/team\" hx-push-url=\"true\">Team</a> <a class=\"p-2 cursor-pointer font-medium text-xl\" href=\"/admin/plans\" hx-push-url=\"true\">Plan</a> <a class=\"p-2 cursor-pointer font-medium text-xl\" hx-post=\"/admin/logout\" hx-push-url=\"true\">Logout</a></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
